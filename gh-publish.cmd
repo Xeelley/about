c23e52deabc25b25f43c3232535665338a5a45a0
Ivan Hrypak <xeelley@gmail.com>
2022-12-28 19:04:09 +0200

    gh-publish.cmd update

--- gh-publish.cmd
+++ gh-publish.cmd
@@ -1,3 +1,9 @@
+:: b/master sould be built before:
+:: - npm run build
+:: 
+:: b/master should be pushed to origin (/dist included):
+:: - git push
+ 
 git checkout master 
 cp ./dist/index.html ./dist/404.html
 git subtree split --prefix dist -b gh-pages 
